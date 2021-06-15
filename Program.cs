e6b445860bf2dfb7970bd47a37209cfe65825097
JohnDoe13 <nick.alzen@live.com>
2021-06-15 11:49:03 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Just my first test-project to GitHub
+            //Nothing fancy.
+            Console.WriteLine("Hello " + args[0]);
+        }
+    }
+}
